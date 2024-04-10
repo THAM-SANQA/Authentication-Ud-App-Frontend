5623e1d232ff990e8dc9fabe278535660e355ea9
THAM-SANQA <ndabat020@gmail.com>
2024-04-10 19:37:50 +0200

    route

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = (props) => {
   const [user, setUser] = useState(null);
 
   const getUser = async () => {
-    const res = await axios.get("/api/auth", {
+    const res = await axios.get("https://authentication-ud-app-backend.onrender.com", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
